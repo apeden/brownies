441f5463102a04a22589bf1fe5426ba7d5d1b66c
apeden <apeden23@gmail.com>
2018-10-02 00:57:16 +0100

    extended the tent class

--- src/brownies/Camp.java
+++ src/brownies/Camp.java
@@ -1,16 +1,19 @@
 package brownies;
 
 /**Class: Camp
- * A camp class will have Brownies and Tents. It will assign 
- * popularities to Brownies on the basis of their friends 
- * and non-friends.  
+ * A camp class will have Brownies and Tents.  
  * 
  * @author Alex Peden
  *
  */
 
-
-public class Camp {
+public class Camp{
+	private Tent[]tents;
+	private Brownie[]brownies;
+	Camp(int num_tents, int num_brownies){
+		tents = new Tent[num_tents];
+		brownies = new Brownie[num_brownies];
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/brownies/Tent.java
+++ src/brownies/Tent.java
@@ -2,10 +2,9 @@ package brownies;
 
 /**
  * This class will create tent objects that have a known capacity
- * i.e the number of brownies they can accommodate and a name which
- * could be a number or an int. The tents also will have an array 
- * of brownies objects. The size of the array depends on the tents 
- * capacity. 
+ * i.e the number of brownies they can accommodate and a number as a
+ * label. The tents also will have an array of brownie objects.
+ *  The size of the array depends on the tents capacity. 
  * 
  * @author Alex Peden
  *
@@ -13,11 +12,12 @@ package brownies;
 
 public class Tent {
 	
-	private int name;
-	private int capacity;
+	private final int name;
+	private final int capacity;
 	private double happiness;
+	private Brownie[] occupants;
 	
-	/**Creates a tent with a number label and a capacity
+	/**Creates a tent with a number label and a capacity for occupants
 	 *
 	 * @param name       the number label of the tent
 	 * @param capacity   the number of brownies that can fit in the tent
@@ -26,13 +26,42 @@ public class Tent {
 	Tent(int tent_name, int tent_capacity){
 		name = tent_name;
 		capacity = tent_capacity;
+		occupants = new Brownie[capacity];
 	}
 
-	/**Returns the tent name as a string identifier*/
+	/** Method: getCapacity   
+	 * Returns the capacity of the tent 
+	 * 
+	 * @return capacity
+	 */
+	public int getCapacity() {
+		return capacity;
+	}
+	
+	/** Method: getHappiness   
+	 * Returns the happiness of the tent
+	
+	 * @return happiness
+	 */
+	public double getHappiness() {
+		return happiness;
+	}
+	
+	/** Method: getOccupants   
+	 * Returns the occupants of the tent
+	 * 
+	 * @return occupants
+	 */	
+	public Brownie[] getOccupants() {
+		return occupants;
+	}
+		
+	/** Method: toString   
+	 * Returns the tent name as a string identifier */
 	public String toString() {
 		String string_name;
 		string_name = Integer.toString(name);
-		return "This is tent number " + string_name;
+		return "Tent number " + string_name;
 	}	
 	
 	public static void main(String[] args) {
